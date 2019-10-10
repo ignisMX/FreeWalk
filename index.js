d771ee4a3b19e54078944086ba7d30d511c32738
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2019-10-10 15:33:54 -0500

    comments

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function freeWalk(steps){
+    //Split String 
     var moves = steps.split("");
     var steps = 0, max = 0, positive = "";
+    //Loop over every element on array
     moves.forEach(element => {
-
+        //Defines positive direction.Every time return to origin, positive direction
+        //can change,  
         if(steps == 0) positive = element;
+
         if(positive == element)
             steps ++;
         else
             steps --;
+        
         if(steps > max) max = steps;
     });
     return max;
